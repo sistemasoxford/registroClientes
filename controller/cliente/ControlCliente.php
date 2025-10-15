b94f0728e5795c41081db44c89f092de6593909c
Juan Arango <auxsistemas2@oxfordjeans.com>
2025-10-15 11:45:22 -0500

    se agrega campo para ver si actualizo o inserto datos

--- controller/cliente/ControlCliente.php
+++ controller/cliente/ControlCliente.php
@@ -4,18 +4,21 @@ require_once BASE_PATH . '/config/config.php';
 require_once(QUERY_OBJECT_PATH . 'objectInsert.php');
 require_once(QUERY_OBJECT_PATH . 'objectUpdate.php');
 
-class ControlCliente {
+class ControlCliente
+{
     var $objCliente;
     var $objOtp;
     private $objControlConexion;
 
-    function __construct($objCliente = null, $objOtp = null) {
+    function __construct($objCliente = null, $objOtp = null)
+    {
         $this->objCliente = $objCliente;
         $this->objOtp = $objOtp;
         $this->objControlConexion = ControlConexion::getInstance();
     }
 
-    function registrarCliente(){
+    function registrarCliente()
+    {
         $tDocumento = $_SESSION['cliente']['tDocumento'];
         $documentoOriginal = $this->objCliente->getDocumento();
         $documento = $_SESSION['cliente']['documento'];
@@ -31,57 +34,69 @@ class ControlCliente {
         $terminos = $_SESSION['cliente']['terminos'];
         $otp = $this->objOtp->getOtp();
         $medioEnvio = $_SESSION['cliente']['medioEnvio'];
+
+        // if ($_SESSION['urlOtp'] == 1) {
+        //     $tipoRegistro = 'Insert';
+        // } else {
+        //     $tipoRegistro = 'Update';
+        // }
+
+        if ($_SESSION['urlOtp'] != 1) {
+            $tipoRegistro = 'Update';
+        } else {
+            $tipoRegistro = 'Insert';
+        }
         // Conexión a la base de datos
         $queryUpdate = new QueryUpdate();
         $comandoSql = $queryUpdate->table("habeas_data")
-                                  ->set(["tDocumento" => $tDocumento, "id" => $documento, "nombre" => $nombres, "apellido" => $apellidos, "sexo" => $sexo, "fecha_nacimiento" => $fechaNacimiento, "email" => $email, "telefono" => $telefono, "direccion" => $direccion, "ciudad" => $ciudad, "region" => $region, "terminos" => $terminos, "medio_envio" => $medioEnvio])
-                                  ->where("id = '$documentoOriginal' AND otp = '$otp'");
+            ->set(["tDocumento" => $tDocumento, "id" => $documento, "nombre" => $nombres, "apellido" => $apellidos, "sexo" => $sexo, "fecha_nacimiento" => $fechaNacimiento, "email" => $email, "telefono" => $telefono, "direccion" => $direccion, "ciudad" => $ciudad, "region" => $region, "terminos" => $terminos, "medio_envio" => $medioEnvio, "tipo_registro" => $tipoRegistro])
+            ->where("id = '$documentoOriginal' AND otp = '$otp'");
 
-        try{
+        try {
             $this->objControlConexion->abrirBd();
             $recordSet = $this->objControlConexion->ejecutarComandoSql($comandoSql);
 
-            if($recordSet > 0){
+            if ($recordSet > 0) {
                 return ["success" => true, "message" => "Usuario creado con exito."];
-            }else{
+            } else {
                 return ["success" => false, "message" => "Error al crear el usuario. Por favor, intente de nuevo."];
             }
-
-        }catch(Exception $e){
+        } catch (Exception $e) {
             throw new Exception("Error durante la conexión a la base de datos: " . $e->getMessage());
-        }finally{
+        } finally {
             $this->objControlConexion->cerrarBd();
         }
     }
 
-    function registraOtp(){
+    function registraOtp()
+    {
         $documento = $this->objCliente->getDocumento();
         $otp = $this->objOtp->getOtp();
 
         // Conexión a la base de datos
         $queryInsert = new QueryInsert();
         $comandoSql = $queryInsert->into("habeas_data")
-                                  ->columns(["id", "otp"])
-                                  ->values([$documento, $otp]);
+            ->columns(["id", "otp"])
+            ->values([$documento, $otp]);
 
-        try{
+        try {
             $this->objControlConexion->abrirBd();
             $recordSet = $this->objControlConexion->ejecutarComandoSql($comandoSql);
 
-            if($recordSet > 0){
+            if ($recordSet > 0) {
                 return ["success" => true, "message" => "Usuario creado con exito."];
-            }else{
+            } else {
                 return ["success" => false, "message" => "Error al subir el archivo. Por favor, intente de nuevo con otro tipo de archivo."];
             }
-
-        }catch(Exception $e){
-            throw new Exception("Error durante la conexión a la base de datos: " . $e->getMessage());
-        }finally{
+        } catch (Exception $e) {
+            throw new Exception("Error durante la conexión a la base de datos: " .  $e->getMessage());
+        } finally {
             $this->objControlConexion->cerrarBd();
         }
     }
 
-    function guardarDatos() {
+    function guardarDatos()
+    {
         // Asegurar que la sesión esté activa
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
@@ -123,5 +138,4 @@ class ControlCliente {
 
         return $guardado;
     }
-
 }
